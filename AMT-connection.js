cdd59aacba2ef2d0701b09f04aaa19359c17c5c4
Sanjana Tripathi <sanjanatripathi@Sanjanas-MacBook-Pro.local>
2019-10-14 19:52:54 -0500

    added gitignore and other npm package files

---
+++ AMT-connection.js
@@ -0,0 +1,27 @@
+var AWS = require('aws-sdk');
+var region = 'us-east-1';
+var aws_access_key_id = 'AKIAJITNCMOI7Y3ND7JQ';
+var aws_secret_access_key = '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';
+
+AWS.config = {
+    "accessKeyId": aws_access_key_id,
+    "secretAccessKey": aws_secret_access_key,
+    "region": region,
+    "sslEnabled": 'true'
+};
+
+var endpoint = 'https://mturk-requester-sandbox.us-east-1.amazonaws.com';
+
+// Uncomment this line to use in production
+// endpoint = 'https://mturk-requester.us-east-1.amazonaws.com';
+
+var mturk = new AWS.MTurk({ endpoint: endpoint });
+
+// This will return $10,000.00 in the MTurk Developer Sandbox
+mturk.getAccountBalance(function(err, data){
+    console.log(data.AvailableBalance);
+});
+
+
+// var aws_access_key_id = 'AKIAJMJGXPF6EQ5EW52A';
+// var aws_secret_access_key = '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';
